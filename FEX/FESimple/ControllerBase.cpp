9d47abf3de931125f2ec12a44c407f4c85622cb2
fancyzero <fancyzero@live.cn>
2013-06-27 09:00:02 +0800

    整理工程结构

---
+++ FEX/FESimple/ControllerBase.cpp
@@ -0,0 +1,9 @@
+//
+//  InputDevice.cpp
+//  FEX
+//
+//  Created by Fancyzero on 13-6-26.
+//
+//
+
+#include "InputDevice.h"
---
+++ FEX/FESimple/ControllerBase.h
@@ -0,0 +1,14 @@
+//
+//  InputDevice.h
+//  FEX
+//
+//  Created by Fancyzero on 13-6-26.
+//
+//
+
+#ifndef __FEX__InputDevice__
+#define __FEX__InputDevice__
+
+#include <iostream>
+
+#endif /* defined(__FEX__InputDevice__) */
